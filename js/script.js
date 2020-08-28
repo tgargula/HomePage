6e98952e76213b48b5f4f81ecec9e14804c5814a
Tomek Gargula <tgargula@student.agh.edu.pl>
2020-08-28 14:04:44 +0200

    Create separate class for main container

--- js/script.js
+++ js/script.js
@@ -25,130 +25,135 @@ const freeSpaceHorizontally = 200;
 const extendedTileWidth = 200 + 2 * minTileMarginVertically;
 const extendedTileHeight = 250 + 2 * minTileMarginHorizontally;
 
-let tilesInRow;
-let rows;
-let pages;
-let currentPage = 1;
+class DefaultContainer {
+    rows;
+    tilesInRow;
+    pages;
 
-function updateInfo() {
-    // Calculate current values
-    tilesInRow = Math.floor((window.innerWidth - freeSpaceVertically) / extendedTileWidth);
-    rows = Math.max(Math.floor((window.innerHeight - freeSpaceHorizontally) / extendedTileHeight), 0);
-    pages = Math.ceil(tiles.length / (tilesInRow * rows));
-
-    // Correct currentPage and pageCounter if necessary
-    if (currentPage > pages)
-        currentPage = pages;
-    if (pages === 1 || pages === Infinity)
-        pageCounter.classList.add("invisible");
-    else pageCounter.classList.remove("invisible");
-
-    // update pageCounter
-    pageCounter.innerHTML = currentPage + " / " + pages;
-}
-
-function updateMargins() {
-    for (const tile of container.getElementsByClassName("tile")) {
-        tile.style.marginLeft = tile.style.marginRight =
-            Math.floor(minTileMarginVertically +
-                (window.innerWidth - freeSpaceVertically) % extendedTileWidth / (2 * tilesInRow)) + "px";
-        tile.style.marginTop = tile.style.marginBottom =
-            Math.floor(minTileMarginHorizontally +
-                (window.innerHeight - freeSpaceHorizontally) % extendedTileHeight / (2 * rows)) + "px";
+    constructor() {
+        this.currentPage = 1;
     }
-}
 
-function updateArrows() {
-    if (currentPage === 1)  // First page
-        leftArrow.classList.add("invisible");
-    else leftArrow.classList.remove("invisible");
-    if (currentPage === pages)  // Last page
-        rightArrow.classList.add("invisible");
-    else rightArrow.classList.remove("invisible");
-    if (pages === Infinity) { // Make arrows invisible iff there are no tiles
-        leftArrow.classList.add("invisible");
-        rightArrow.classList.add("invisible");
+    updateInfo() {
+        // Calculate current values
+        this.tilesInRow = Math.floor((window.innerWidth - freeSpaceVertically) / extendedTileWidth);
+        this.rows = Math.max(Math.floor((window.innerHeight - freeSpaceHorizontally) / extendedTileHeight), 0);
+        this.pages = Math.ceil(tiles.length / (this.tilesInRow * this.rows));
+
+        // Correct currentPage and pageCounter if necessary
+        if (this.currentPage > this.pages)
+            this.currentPage = this.pages;
+        if (this.pages === 1 || this.pages === Infinity)
+            pageCounter.classList.add("invisible");
+        else pageCounter.classList.remove("invisible");
+
+        // update pageCounter
+        pageCounter.innerHTML = this.currentPage + " / " + this.pages;
     }
-}
 
-function addTiles() {
-    const end = Math.min(tiles.length, currentPage * rows * tilesInRow);
-    for (let index = (currentPage - 1) * rows * tilesInRow; index < end; index++) {
-        // Create div
-        const div = document.createElement("div");
-        div.setAttribute("class", "tile");
-
-        // Create link
-        const a = document.createElement("a");
-        a.setAttribute("class", "link");
-        a.setAttribute("href", tiles[index].href);
-
-        // Create image
-        const img = document.createElement("img");
-        img.setAttribute("class", "image");
-        img.setAttribute("src", "images/" + tiles[index].src);
-        img.setAttribute("alt", "");
-
-        // Create tile name
-        const p = document.createElement("p");
-        p.setAttribute("class", "name");
-        p.innerHTML = tiles[index].name;
-
-        // Join all elements and add to the container
-        a.appendChild(img);
-        a.appendChild(p);
-        div.appendChild(a);
-        container.appendChild(div);
+    updateMargins() {
+        for (const tile of container.getElementsByClassName("tile")) {
+            tile.style.marginLeft = tile.style.marginRight =
+                Math.floor(minTileMarginVertically +
+                    (window.innerWidth - freeSpaceVertically) % extendedTileWidth / (2 * this.tilesInRow)) + "px";
+            tile.style.marginTop = tile.style.marginBottom =
+                Math.floor(minTileMarginHorizontally +
+                    (window.innerHeight - freeSpaceHorizontally) % extendedTileHeight / (2 * this.rows)) + "px";
+        }
     }
-}
 
-function removeTiles() {
-    while (container.firstChild)
-        container.removeChild(container.lastChild);
-}
-
-function update() {
-    updateInfo();
-    updateArrows();
-    removeTiles();
-    addTiles();
-    updateMargins();
-}
-
-function moveRight() {
-    if (currentPage < pages) {
-        currentPage++;
-        update();
+    updateArrows() {
+        if (this.currentPage === 1)  // First page
+            leftArrow.classList.add("invisible");
+        else leftArrow.classList.remove("invisible");
+        if (this.currentPage === this.pages)  // Last page
+            rightArrow.classList.add("invisible");
+        else rightArrow.classList.remove("invisible");
+        if (this.pages === Infinity) { // Make arrows invisible iff there are no tiles
+            leftArrow.classList.add("invisible");
+            rightArrow.classList.add("invisible");
+        }
     }
-}
 
-function moveLeft() {
-    if (currentPage > 1) {
-        currentPage--;
-        update();
+    addTiles() {
+        const end = Math.min(tiles.length, this.currentPage * this.rows * this.tilesInRow);
+        for (let index = (this.currentPage - 1) * this.rows * this.tilesInRow; index < end; index++) {
+            // Create div
+            const div = document.createElement("div");
+            div.setAttribute("class", "tile");
+
+            // Create link
+            const a = document.createElement("a");
+            a.setAttribute("class", "link");
+            a.setAttribute("href", tiles[index].href);
+
+            // Create image
+            const img = document.createElement("img");
+            img.setAttribute("class", "image");
+            img.setAttribute("src", "images/" + tiles[index].src);
+            img.setAttribute("alt", "");
+
+            // Create tile name
+            const p = document.createElement("p");
+            p.setAttribute("class", "name");
+            p.innerHTML = tiles[index].name;
+
+            // Join all elements and add to the container
+            a.appendChild(img);
+            a.appendChild(p);
+            div.appendChild(a);
+            container.appendChild(div);
+        }
+    }
+
+    removeTiles() {
+        while (container.firstChild)
+            container.removeChild(container.lastChild);
+    }
+
+    update() {
+        this.updateInfo();
+        this.updateArrows();
+        this.removeTiles();
+        this.addTiles();
+        this.updateMargins();
+    }
+
+    moveRight() {
+        if (this.currentPage < this.pages) {
+            this.currentPage++;
+            this.update();
+        }
+    }
+
+    moveLeft() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+            this.update();
+        }
     }
 }
 
 window.onload = function () {
     // INIT
-    request.onload = update;
-
-    rightArrow.addEventListener("click", moveRight);
-    leftArrow.addEventListener("click", moveLeft);
+    let defaultContainer = new DefaultContainer();
+    request.onload = function () { defaultContainer.update(); };
+    rightArrow.addEventListener("click", function () { defaultContainer.moveRight(); });
+    leftArrow.addEventListener("click", function() { defaultContainer.moveLeft(); });
 
     // Change layout on resize
     window.addEventListener("resize", function () {
-        const tmp = rows * tilesInRow;
-        updateInfo();
-        updateArrows();
+        const tmp = defaultContainer.rows * defaultContainer.tilesInRow;
+        defaultContainer.updateInfo();
+        defaultContainer.updateArrows();
 
         // Update layout if necessary
-        if (tmp !== rows * tilesInRow) {
-            removeTiles();
-            addTiles();
+        if (tmp !== defaultContainer.rows * defaultContainer.tilesInRow) {
+            defaultContainer.removeTiles();
+            defaultContainer.addTiles();
         }
 
-        updateMargins();
+        defaultContainer.updateMargins();
     });
 
     window.addEventListener("keydown", function (e) {
@@ -166,18 +171,18 @@ window.onload = function () {
         } else {
             switch (e.key) {
                 case "ArrowRight":
-                    moveRight();
+                    defaultContainer.moveRight();
                     break;
                 case "ArrowLeft":
-                    moveLeft();
+                    defaultContainer.moveLeft();
                     break;
                 case "Home":
-                    currentPage = 1;
-                    update();
+                    defaultContainer.currentPage = 1;
+                    defaultContainer.update();
                     break;
                 case "End":
-                    currentPage = pages;
-                    update();
+                    defaultContainer.currentPage = defaultContainer.pages;
+                    defaultContainer.update();
                     break;
                 case "Enter":
                 case "ArrowDown":
@@ -193,9 +198,9 @@ window.onload = function () {
                     else {
                         const key = parseInt(e.key);
                         if (!isNaN(key)) {      // if number
-                            if (key <= pages && key > 0) {
-                                currentPage = key;
-                                update();
+                            if (key <= defaultContainer.pages && key > 0) {
+                                defaultContainer.currentPage = key;
+                                defaultContainer.update();
                             }
                         }
                     }
